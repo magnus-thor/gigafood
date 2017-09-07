e2a9b8e12727119a07bea44cb7b441f49f4680f2
j0sv <svensson.jonas@gmail.com>
2017-09-07 12:28:05 +0200

    remove link in read me file (#2)

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # gigafood
 CraftAcademy - Summer 2017 Final Project
-https://www.pivotaltracker.com/n/projects/2104405
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,14 @@ Bundler.require(*Rails.groups)
 module Gigafood
   class Application < Rails::Application
     config.load_defaults 5.1
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+    end
 
     config.generators.system_tests = nil
   end
