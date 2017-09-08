8759cb6f9f929ae1e1ac7ae1f2059021e9d3bda1
faraznaeem <faraznaeem@gmail.com>
2017-09-08 14:05:05 +0200

    Delete unused steps

--- features/step_definitions/dishes_steps.rb
+++ features/step_definitions/dishes_steps.rb
@@ -5,7 +5,6 @@ Given(/^the following dishes exist:$/) do |table|
 end
 
 When(/^I press "([^"]*)" for dish "([^"]*)"$/) do |link, dish_name|
-  #pending # Write code here that turns the phrase above into concrete actions
   dish = Dish.find_by(name: dish_name)
   within("#dish_#{dish.id}") do
     click_link_or_button link
--- features/step_definitions/generic_steps.rb
+++ features/step_definitions/generic_steps.rb
@@ -2,10 +2,6 @@ Then(/^show me the page$/) do
   save_and_open_page
 end
 
-When(/^I press "([^"]*)" on "([^"]*)"$/) do |arg1, arg2|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
 When(/^I confirm popup$/) do
   page.driver.browser.switch_to.alert.accept
 end
