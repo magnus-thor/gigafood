980dccc5cd8cd3a2df5dacdfeac371e3080f05b1
tochman <thomas@craftacademy.se>
2017-09-13 17:29:50 +0200

    changes OrderItem settings to use SEK as default skips _cents posfix on price table

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,6 +1,15 @@
 
 MoneyRails.configure do |config|
   config.default_currency = :sek
+
+  config.amount_column = {prefix: '', # column name prefix
+                          postfix: '', # column name  postfix
+                          column_name: nil, # full column name (overrides prefix, postfix and accessor name)
+                          type: :integer, # column type
+                          present: true, # column will be created
+                          null: false, # other options will be treated as column options
+                          default: 0
+  }
 end
 
 
--- spec/models/order_item_spec.rb
+++ spec/models/order_item_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe OrderItem, type: :model do
   it { is_expected.to have_db_column :quantity }
   it { is_expected.to have_db_column :item_id }
   it { is_expected.to have_db_column :item_type }
-  it { is_expected.to have_db_column :price_cents }
+  it { is_expected.to have_db_column :price }
   it { is_expected.to have_db_column :price_currency }
   it { is_expected.to have_db_column :created_at }
   it { is_expected.to have_db_column :updated_at }
