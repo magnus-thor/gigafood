646cc612b8deaf401c5c4dacfd792d2b62caf11d
tochman <thomas@craftacademy.se>
2017-09-14 01:42:45 +0200

    changes to Rails.root in step def and starts to loose my patiance

--- features/step_definitions/pdf_steps.rb
+++ features/step_definitions/pdf_steps.rb
@@ -4,7 +4,7 @@ end
 
 And(/^the invoice should contain "([^"]*)"$/) do |content|
 
-  remote_pdf = open('spec/fixtures/tmp/tmp.pdf', 'wb') do |file|
+  remote_pdf = open(Rails.root.join('spec', 'fixtures', 'tmp', 'tmp.pdf'), 'wb') do |file|
     file << open(@order.attachments.first.file.url).read
   end
   pdf = PDF::Inspector::Text.analyze_file(remote_pdf)
