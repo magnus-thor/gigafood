9898fcc136d59840e557361fe7ae3a0ef19ecc56
tochman <thomas@craftacademy.se>
2017-09-14 00:11:14 +0200

    adds s3 storage to attachment - remember to add aws mock in test env

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,5 +1,14 @@
 class Attachment < ApplicationRecord
-  has_attached_file :file
+  has_attached_file :file,
+                    storage: :s3,
+                    s3_credentials: {
+                        bucket: ENV['AWS_BUCKET'],
+                        access_key_id: ENV['AWS_ACCESS_KEY'],
+                        secret_access_key: ENV['AWS_SECRET_KEY'],
+                        s3_region: ENV['AWS_REGION'],
+                        url: :s3_domain_url,
+                        s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
+                    },
   validates_attachment_presence :file
   validates_attachment_content_type :file,
                                     content_type: ['application/pdf']
