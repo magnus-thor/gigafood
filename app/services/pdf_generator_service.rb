df3f71672c71044e76fcd71ac98c99c1f6c6f818
tochman <thomas@craftacademy.se>
2017-09-14 17:56:30 +0200

    Still WIP

--- app/services/pdf_generator_service.rb
+++ app/services/pdf_generator_service.rb
@@ -54,7 +54,7 @@ class PdfGeneratorService
     data << ['','','','']
     # A4 > 595.28 x 841.89
 
-    pdf.table(data, width: 520, column_widths: [280, 60 ,30, 150]) do |table|
+    pdf.table(data, width: 520, column_widths: [300, 60 ,30, 130]) do |table|
       #table.cells.padding = 10
       #table.cells.borders = []
 
@@ -65,21 +65,31 @@ class PdfGeneratorService
 
 
       table.before_rendering_page do |page|
+        # First row has a top border and no bottom border - no side borders
         page.row(0).border_top_width = 2
-        #page.row(1).border_top_width = 2
-        #page.row(0).border_left_width = 2
-        #page.row(0).border_right_width = 2
-        #page.row(1).column(1).border_left_width = 2
-        #page.row(1).border_right_width = 2
-        #page.row(-1).border_bottom_width = 2
-        #page.row(-2).border_bottom_width = 2
-        #page.row(-1).border_right_width = 2
-        #page.row(-1).border_left_width = 2
-        #page.row(-2).border_right_width = 2
-        #page.row(-2).column(-1).border_left_width = 2
-        page.rows(2..-2).borders = []
+        page.row(0).border_bottom_width = 0
+        page.row(0).border_left_width = 0
+        page.row(0).border_right_width = 0
+
+        # Last row has a bottom border and no top border  - no side borders
+        page.row(-1).border_bottom_width = 2
+        page.row(-1).border_top_width = 0
+        page.row(-1).border_right_width = 0
+        page.row(-1).border_left_width = 0
+
+        # The order details have no borders at all
+        page.rows(1..-2).borders = []
+
+        # Item price column aligns to right
+        page.column(2).align = :right
+        # Quantity column is centered
+        page.column(-2).align = :center
+        # Subtotal column aligns to right
         page.column(-1).align = :right
-        page.row(-1).font_style = :bold
+        # Header row is bold
+        page.row(1).font_style = :bold
+        # Order total row is bold
+        page.row(-2).font_style = :bold
       end
 
     end
@@ -87,7 +97,7 @@ class PdfGeneratorService
 
     #Footer
 
-    pdf.bounding_box([pdf.bounds.right - 550, pdf.bounds.bottom + 30], width: 800, height: 70) do
+    pdf.bounding_box([pdf.bounds.right - 550, pdf.bounds.bottom + 50], width: 800, height: 70) do
       pdf.text 'Food for Billions Sweden AB', align: :left, size: 9, style: :bold
       pdf.text '559040-1435', align: :left, size: 8
       pdf.text 'Tegelbacken 4A', align: :left, size: 8
@@ -97,7 +107,7 @@ class PdfGeneratorService
       pdf.text 'invoice@gigafood.se', align: :left, size: 8
     end
 
-    pdf.bounding_box([pdf.bounds.right - 250, pdf.bounds.bottom + 30], width: 800, height: 70) do
+    pdf.bounding_box([pdf.bounds.right - 250, pdf.bounds.bottom + 50], width: 800, height: 70) do
       pdf.text 'Handelsbanken', align: :left, size: 8
       pdf.text 'Bankgiro: 862-9305', align: :left, size: 8
       pdf.text 'Swish: 1230486472', align: :left, size: 8
@@ -111,6 +121,7 @@ class PdfGeneratorService
     # TODO: This is WIP. Needs to be extracted to a module
     generated_file = File.open(Rails.root.join(filename))
     if @order.attachments.create(file: generated_file, file_type: :invoice)
+      # TODO: Delete the generated file. Disabled while testing
       # if File.exist?(generated_file)
       #   File.delete(generated_file)
       # end
