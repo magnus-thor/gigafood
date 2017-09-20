16a37f4c1f3f606e50c8115d37a2a8c41d14ac0b
j0sv <svensson.jonas@gmail.com>
2017-09-20 08:45:54 +0200

    Add validation to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,22 @@
 class Order < ApplicationRecord
   acts_as_shopping_cart_using :order_item
+  validates :delivery_date,       presence: true, :if => :submitted?
+  validates :status,              presence: true, :if => :submitted?
+  validates :delivery_method,     presence: true, :if => :submitted?
+  validates :billing_company,     presence: true, :if => :submitted?
+  validates :billing_org_nr,      presence: true, :if => :submitted?
+  validates :billing_address,     presence: true, :if => :submitted?
+  validates :billing_postal_code, presence: true, :if => :submitted?
+  validates :billing_city,        presence: true, :if => :submitted?
+  validates :billing_phone,       presence: true, :if => :submitted?
+  validates :billing_email,       presence: true, :if => :submitted?
+
   SWEDISH_VAT = 12
   STANDARD_PAYMENT_TIME_IN_DAYS = 30
 
   has_many :attachments, dependent: :destroy
-  
+
+
   def tax_pct
     Order::SWEDISH_VAT
   end
@@ -19,5 +31,9 @@ class Order < ApplicationRecord
 
   def has_menu?
     Order.find(self.id).attachments.exists?(file_type: 'menu')
+  end
+
+  def submitted?
+    status == 'submitted'
   end
 end
