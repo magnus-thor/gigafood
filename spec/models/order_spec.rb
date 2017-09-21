bac55b522ff6f5f888d2000f09f5a5af4f91df81
tochman <thomas@craftacademy.se>
2017-09-21 15:53:52 +0200

    alternative way to test validations

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -30,6 +30,34 @@ RSpec.describe Order, type: :model do
     it { is_expected.to have_many :attachments }
   end
 
+  describe 'Validations' do
+    let!(:attributes_to_validate) { [:billing_company,
+                                     :billing_org_nr,
+                                     :billing_address,
+                                     :billing_postal_code,
+                                     :billing_city,
+                                     :billing_phone,
+                                     :billing_email] }
+    context 'order is NOT submitted' do
+      it 'skips validation of billing attributes' do
+        attributes_to_validate.each do |attribute|
+          is_expected.to_not validate_presence_of(attribute)
+        end
+      end
+
+    end
+
+    context 'order is submitted' do
+      before { allow(subject).to receive(:submitted?).and_return(true) }
+
+      it 'validates billing attributes' do
+        attributes_to_validate.each do |attribute|
+          is_expected.to validate_presence_of(attribute)
+        end
+      end
+    end
+  end
+
   describe '#set_payment_due_date' do
     before { subject.set_payment_due_date }
 
@@ -44,7 +72,7 @@ RSpec.describe Order, type: :model do
   end
 
   describe '#has_invoice?' do
-    let(:item) { create(:dish)}
+    let(:item) { create(:dish) }
 
     before do
       subject.add(item, item.price, 1)
@@ -58,7 +86,7 @@ RSpec.describe Order, type: :model do
   end
 
   describe '#has_menu?' do
-    let(:item) { create(:dish)}
+    let(:item) { create(:dish) }
 
     before do
       subject.add(item, item.price, 1)
@@ -68,80 +96,41 @@ RSpec.describe Order, type: :model do
     it 'returns true if menu exists' do
       expect(subject.has_menu?).to eq true
     end
-
   end
 
-  describe 'Returns false if order is not submitted.' do
-
-    it 'returns false' do
-      expect(subject.submitted?).to eq false
+  describe '#submitted?' do
+    describe 'order is not submitted' do
+      it 'returns false' do
+        expect(subject.submitted?).to eq false
+      end
     end
 
+    describe 'order is submitted' do
+      let(:item) { create(:dish) }
+
+      before do
+        subject.add(item, item.price, 1)
+
+        order_values = {delivery_method: 'delivery',
+                        delivery_name: 'Big corp',
+                        billing_name: 'Big corp 111',
+                        billing_company: 'Big corp 222',
+                        billing_org_nr: '19900101-1234',
+                        billing_address: 'Street 42',
+                        billing_postal_code: '123 45',
+                        billing_city: 'Town',
+                        billing_phone: '555 123 45 66',
+                        billing_email: 'invoce@bigcorp.com',
+                        status: 'submitted'}
+
+        subject.update(order_values)
+      end
+
+      it 'returns true' do
+        expect(subject.submitted?).to eq true
+      end
+    end
   end
-
-  describe 'Returns true if order is submitted and has required attributes.' do
-    let(:item) { create(:dish)}
-
-    before 'Set order to submitted and add required data' do
-      subject.add(item, item.price, 1)
-
-      order_values = {delivery_method: 'delivery', delivery_name: 'Big corp', billing_name: 'Big corp 111',
-                      billing_company: 'Big corp 222', billing_org_nr: '19900101-1234', billing_address: 'Street 42',
-                      billing_postal_code: '123 45', billing_city: 'Town', billing_phone: '555 123 45 66',
-                      billing_email: 'invoce@bigcorp.com', status: 'submitted'}
-
-      subject.update(order_values)
-    end
-
-    it 'returns true if order is submitted' do
-      expect(subject.submitted?).to eq true
-    end
-
-    it 'has a billing_company' do
-      is_expected.to validate_presence_of(:billing_company)
-    end
-
-    it 'has a billing_org_nr' do
-      is_expected.to validate_presence_of(:billing_org_nr)
-    end
-
-    it 'has a billing_address' do
-      is_expected.to validate_presence_of(:billing_address)
-    end
-
-    it 'has a billing_postal_code' do
-      is_expected.to validate_presence_of(:billing_postal_code)
-    end
-
-    it 'has a billing_city' do
-      is_expected.to validate_presence_of(:billing_city)
-    end
-
-    it 'has a billing_phone' do
-      is_expected.to validate_presence_of(:billing_phone)
-    end
-
-    it 'has a billing_email' do
-      is_expected.to validate_presence_of(:billing_email)
-    end
-
-    # it 'has a delivery_method' do
-    #   is_expected.to validate_presence_of(:delivery_method)
-    # end
-    #
-    # it 'has a billing_name' do
-    #   is_expected.to validate_presence_of(:billing_name)
-    # end
-
-  end
-
 end
 
 
-#order_params = {allergies: 'peanuts', delivery_method: 'delivery', delivery_name: 'Big corp',
-#                delivery_address: 'Street 42', delivery_postal_code: '123 45', delivery_city: 'Town',
-#                delivery_floor: '3', delivery_door_code: '1234', delivery_contact_name: 'John Doe',
-#                delivery_contact_phone_number: '555 123 45 66', billing_name: 'Big corp 111',
-#                billing_company: 'Big corp 222', billing_org_nr: '19900101-1234', billing_address: 'Street 42',
-#                billing_postal_code: '123 45', billing_city: 'Town', billing_phone: '555 123 45 66',
-#                billing_email: 'invoce@bigcorp.com', status: 'submitted'}
