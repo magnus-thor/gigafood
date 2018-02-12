417969c50a72d0233dc63ad21920b0c07b851a68
magnus-thor <magnus.thor.ar@gmail.com>
2018-02-12 13:30:05 +0100

    update review order email add logo at top of email setup config.action in test.rb to be able to send email in test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,6 +36,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 
+  config.action_controller.asset_host = 'http://localhost:3000'
+  config.action_mailer.asset_host = config.action_controller.asset_host
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
