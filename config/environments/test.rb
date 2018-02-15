fe5cbd9a7540a2a43b2995aa17ea0ac78a2e6861
magnus-thor <magnus.thor.ar@gmail.com>
2018-02-15 13:46:52 +0100

    commented out mailer code in test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,8 +33,8 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   # config.action_mailer.delivery_method = :test
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 
   # This needs to uncommented to pictures in emails when running tests
   # config.action_controller.asset_host = 'http://localhost:3000'
