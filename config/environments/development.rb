d305a85ed8507d40e40198be8995ef95eb21d9f5
magnus-thor <magnus.thor.ar@gmail.com>
2018-02-15 13:50:31 +0100

    in test.rb changed to delivery_method test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,8 +47,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   # config.action_controller.asset_host = 'http://localhost:3000'
   # config.action_mailer.asset_host = config.action_controller.asset_host
 
