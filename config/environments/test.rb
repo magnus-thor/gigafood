232a259c2056229f83eeb03119d52cf63978698b
magnus-thor <magnus.thor.ar@gmail.com>
2018-02-19 10:37:14 +0100

    changed review order email kept header in center, put the rest to the left

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,13 +32,13 @@ Rails.application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  # config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 
   # This needs to uncommented to pictures in emails when running tests
-  # config.action_controller.asset_host = 'http://localhost:3000'
-  # config.action_mailer.asset_host = config.action_controller.asset_host
+  config.action_controller.asset_host = 'http://localhost:3000'
+  config.action_mailer.asset_host = config.action_controller.asset_host
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
