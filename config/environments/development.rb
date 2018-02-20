71dbba7aa3644060939f0f54d63c6e2f64ad3819
magnus-thor <magnus.thor.ar@gmail.com>
2018-02-20 20:40:30 +0100

    WIP styling the html email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,8 +49,8 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
-  # config.action_controller.asset_host = 'http://localhost:3000'
-  # config.action_mailer.asset_host = config.action_controller.asset_host
+  config.action_controller.asset_host = 'http://localhost:3000'
+  config.action_mailer.asset_host = config.action_controller.asset_host
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
