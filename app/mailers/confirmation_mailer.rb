ae787bb50a53339f43d16d7235e2042d38a62762
magnus-thor <magnus.thor.ar@gmail.com>
2018-02-22 15:09:35 +0100

    extracted partials from emails WIP - still styling the f.. emails

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,15 +1,15 @@
 class ConfirmationMailer < ApplicationMailer
   default from: 'noreply@gigafood.se'
 
-  # before_action :attach_image
+  before_action :attach_image
 
   def review_email(order)
     @order = order
-    #mail(to: @order.billing_email, subject: 'Gigafood Order')
+    mail(to: @order.billing_email, subject: 'Gigafood Order')
   end
 
   def confirmation_email(order)
-    attachments.inline['gigafood_logo.png'] = File.read('app/assets/images/gigafood_logo.png')
+    # attachments.inline['gigafood_logo.png'] = File.read('app/assets/images/gigafood_logo.png')
     @order = order
     @url = 'http://example.com/confirmation'
     @order.status = 'approved'
@@ -28,7 +28,7 @@ class ConfirmationMailer < ApplicationMailer
     mail(to: email, subject: 'Request for delivery from Gigafood')
   end
 
-    # def attach_image
-    #   attachments.inline['gigafood_logo.png'] = File.read('/images/gigafood_logo.png')
-    # end
+    def attach_image
+      attachments.inline['gigafood_logo.png'] = File.read('app/assets/images/gigafood_logo.png')
+    end
 end
